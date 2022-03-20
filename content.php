13f6a7597c98fc776f771f2465cd1229f9f09579
ms-reda <101990151+ms-reda@users.noreply.github.com>
2022-03-20 20:27:31 +0200

    Add files via upload

---
+++ content.php
@@ -0,0 +1,241 @@
+
+<section class="reklamine-pavadinimas">
+        <div class="container">
+            <h1>Reklaminė fotografija</h1>
+        </div>
+    </section>
+    <section class="prekes">
+      
+        <div class="container flex-container">
+            <div class="prekiu-images">
+                <!-- flexboxe vaikus sudėti į div, kitaip neveiks -->
+                <img src="../app/images/preke1.jpg">
+            </div>
+            <div class="prekiu-images">
+                <img src="../app/images/preke2.jpg">
+            </div>
+            <div class="prekiu-images">
+                <img src="../app/images/preke3.jpg">
+            </div>
+            <div class="prekiu-images">
+                <img src="../app/images/preke4.jpg">
+            </div>
+        </div>
+    </section>
+    <!--klientai-->
+    <section class="klientai">
+        <div class="container">
+            <div class="section-heading">
+                <h2>Klientai</h2>
+                <p>Didžiuojamės galėdami dirbti su kompanijomis. Prisitaikome prie kliento poreikių dirbant su prekės
+                    ženklais</p>
+            </div>
+            <div class="section-content flex-container">
+                <div class="klientu-logo">
+                    <img src="../app/images/kauno-grudai.png" alt="kauno-grudai">
+                </div>
+                <div class="klientu-logo">
+                    <img src="../app/images/dhl.png" alt="dhl">
+                </div>
+                <div class="klientu-logo">
+                    <img src="../app/images/vikis.png" alt="vikis">
+                </div>
+                <div class="klientu-logo">
+                    <img src="../app/images/zenitech.png" alt="zenitech">
+                </div>
+                <div class="klientu-logo">
+                    <img src="../app/images/lomista.png" alt="lomista">
+                </div>
+            </div>
+        </div>
+    </section>
+
+<!--service-->
+<div class="service">
+    <div class="paslaugos"><h2>Paslaugos</h2>
+            <div class="paslauga-1">
+                <div class="service-icon">
+                <ion-icon name="camera-outline"></ion-icon>
+                </div>
+                    <div class="testi">
+                    <span>Produktų fotografavimas</span>
+                        <p>Aukštos kokybės daiktų, prekių nuotraukos jūsų e-parduotuvei, žurnalams. E-Parduotuvės daiktų, prekių ir produktų fotografija.
+
+Efektyvi vaizdinė medžiaga bene geriausia marketingo priemonė pritraukti kliento „akį“. Prekių, daiktų ir produktų aukštos kokybės fotografavimo paslaugos.</p> 
+                    </div>
+            </div>
+            <div class="paslauga-2">
+                <div class="testi">
+                    <span>Fotoknygos gamyba ir spausdinimas</span>
+                    <p>Fotoknygos gamyba – paslauga apimanti knygos maketo paruošimą ir spausdinimą. 
+                            Gaminamos unikalios, itin kokybiškos, rankų darbo fotoknygos.
+                            Galima pasirinkti įvairių audinių, odų spalvų viršelius fotoknygos maketui.</p>    
+                </div>
+                <div class="service-icon">
+                    <ion-icon name="albums-outline"></ion-icon>
+                </div>
+            </div>
+    </div>
+</div>
+     <!--galerija-->
+     <section class="gallery">
+        <h2 class="gallery-pavadinimas">
+            Daiktų, prekių ir produktų fotografavimas
+        </h2>
+        <div class="container-mason">
+  <figure>
+  <a data-fancybox="gallery" href="../app/images/galerija/hya1a.jpg">
+  <img src="../app/images/galerija/hya1.jpg" alt="hya5"/></a>
+  </figure>
+
+  <figure>
+  <a data-fancybox="gallery" href="../app/images/galerija/istirpk_001.jpg">
+  <img src="../app/images/galerija/istirpk_001-2.jpg" alt="istirpk1"/></a>
+  </figure>
+
+  <figure>
+  <a data-fancybox="gallery" href="../app/images/galerija/istirpk_002.jpg">
+  <img src="../app/images/galerija/istirpk_002-2.jpg" alt="istirpk2"/></a>
+  </figure>
+
+  <figure>
+  <a data-fancybox="gallery" href="../app/images/galerija/istirpk_003.jpg">
+  <img src="../app/images/galerija/istirpk_003-2.jpg" alt="istirpk3"/></a>
+  </figure>
+
+  <figure>
+  <a data-fancybox="gallery" href="../app/images/galerija/istirpk_004.jpg">
+  <img src="../app/images/galerija/istirpk_004-2.jpg" alt="istirpk4"/></a>
+  </figure>
+
+  <figure>
+  <a data-fancybox="gallery" href="../app/images/galerija/hya2a.jpg">
+  <img src="../app/images/galerija/hya2.jpg" alt="hya2"/></a>
+  </figure>
+
+  <figure>
+  <a data-fancybox="gallery" href="../app/images/galerija/hya3a.jpg">
+  <img src="../app/images/galerija/hya3.jpg" alt="hya3"/></a>
+  </figure>
+
+  <figure>
+  <a data-fancybox="gallery" href="../app/images/galerija/style_001.jpg">
+  <img src="../app/images/galerija/style_001-2.jpg" alt="istirpk1"/></a>
+  </figure>
+
+  <figure>
+  <a data-fancybox="gallery" href="../app/images/galerija/style_002.jpg">
+  <img src="../app/images/galerija/style_002-2.jpg" alt="style2"/></a>
+  </figure>
+
+  <figure>
+  <a data-fancybox="gallery" href="../app/images/galerija/style_003.jpg">
+  <img src="../app/images/galerija/style_003-2.jpg" alt="style3"/></a>
+  </figure>
+
+  <figure>
+  <a data-fancybox="gallery" href="../app/images/galerija/style_004.jpg">
+  <img src="../app/images/galerija/style_004-2.jpg" alt="style4"/></a>
+  </figure>
+
+  <figure>
+  <a data-fancybox="gallery" href="../app/images/galerija/hya4a.jpg">
+  <img src="../app/images/galerija/hya4.jpg" alt="hya4"/></a>
+  </figure>
+
+  <figure>
+  <a data-fancybox="gallery" href="../app/images/galerija/hya5a.jpg">
+  <img src="../app/images/galerija/hya5.jpg" alt="hya5"/></a>
+  </figure>
+
+  <figure>
+  <a data-fancybox="gallery" href="../app/images/galerija/style_005.jpg">
+  <img src="../app/images/galerija/style_005-2.jpg" alt="style5"/></a>
+  </figure>
+  <figure>
+  <a data-fancybox="gallery" href="../app/images/galerija/style_006.jpg">
+  <img src="../app/images/galerija/style_006-2.jpg" alt="style6"/></a>
+  </figure>
+          </div>
+          <a href="https://redaruzel.com/paslaugos/daiktu-prekiu-ir-produktu-fotografavimas/">
+            <button>Daugiau nuotraukų</button></a>
+</section>
+
+    <!--atsiliepimai-->
+    <section class="atsiliepimai">
+    <h2 class="mt-12 mb-8 px-6 text-center text-lg md:text-2xl font-semibold">
+        Klientai kalba
+      </h2>
+<div id="quoteCarousel" class="carousel text-lg">
+  <div class="carousel__viewport px-20">
+    <figure class="carousel__slide w-1/3">
+      <blockquote class="bg-white shadow-lg p-8 mb-8">
+        <p>
+          What can I say? You absolutely have to try this thing otherwise you're
+          going to miss out. I promise you won't regret it.
+        </p>
+      </blockquote>
+      <figcaption class="font-semibold">Jane Doe CEO, SpaceX</figcaption>
+    </figure>
+    <figure class="carousel__slide w-1/3">
+      <blockquote class="bg-white shadow-lg p-8 mb-8">
+        <p>
+          Great product. Great team. I have never been more excited to recommend
+          a product to my friends.
+        </p>
+      </blockquote>
+      <figcaption class="font-semibold">John Doe CTO, Tesla</figcaption>
+    </figure>
+    <figure class="carousel__slide w-1/3">
+      <blockquote class="bg-white shadow-lg p-8 mb-8">
+        <p>
+          This is an amazing product! I am blown away by the number of features
+          and the customer service. Highly recommend.
+        </p>
+      </blockquote>
+      <figcaption class="font-semibold">Rita Doe COO, SolarCity</figcaption>
+    </figure>
+    <figure class="carousel__slide w-1/3">
+      <blockquote class="bg-white shadow-lg p-8 mb-8">
+        <p>Fantastic, I'm totally blown away by this product!</p>
+      </blockquote>
+      <figcaption class="font-semibold">Joe Fletcher, Google</figcaption>
+    </figure>
+    <figure class="carousel__slide w-1/3">
+      <blockquote class="bg-white shadow-lg p-8 mb-8">
+        <p>
+          This is unbelievable. After using this product my buisness
+          skyrocketed!
+        </p>
+      </blockquote>
+      <figcaption class="font-semibold">Tim Keeley, Facebook</figcaption>
+    </figure>
+  </div>
+</div>
+    </section>
+
+    <!--kontaktų forma-->
+    <section class="contact">
+        <div class="container">
+            <div class="section-heading">
+                <h2>Susisiekite</h2>
+                <p>Nežinote, nuo ko pradėti planuojant reklaminę fotosesiją arba norite įamžinti įmonės renginį?
+                    Parašykite ir sukursime idėjas kartu!</p>
+            </div>
+            <div class="section-content">
+                <div class="contact-form">
+                    <form id="contact" action="index.php" method="post">
+                        <div class="input-row">
+                        <input type="text" name="vardas" placeholder="Vardas" required>
+                        <input type="email" name="email" placeholder="El.paštas" required>
+                        </div>
+                        <!--kad duotu min 8 row message rasyti. required, kad duomenys butu uzpildyti-->
+                        <textarea name="message" placeholder="Žinutė" rows="17" required></textarea>
+                        <button name="submit" type="submit" id="contact-submit">Siųsti</button>
+                    </form>
+                    <!--class="btn btn-form". class="btn btn-form" klase panaikinta pakeisti css btn aprasymus i #contact button[type="submit"]-->
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
